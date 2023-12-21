e1169b6dc3ebe346da2beba07c961e22226b7228
yusuf <yusuffdemirci10@gmail.com>
2023-12-21 21:48:14 +0300

    Input component and FormWrapper components created

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,14 @@
+"use-client";
+
+import { Fragment } from "react";
+import Headline from "../components/Headline";
+
+const RegisterForm = () => {
+  return (
+    <Fragment>
+      <Headline title="Kayıt Ol" />
+    </Fragment>
+  );
+};
+
+export default RegisterForm;
---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import Container from "../components/Container";
+import FormWrapper from "../components/FormWrapper";
+
+const Register = () => {
+  return (
+    <Container>
+      <FormWrapper></FormWrapper>
+    </Container>
+  );
+};
+
+export default Register;
